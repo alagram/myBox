bc381997a8022e9a2718c5c89a9ccecf53075c42
Albert Agram <albert.agram@gmail.com>
2014-06-09 14:48:12 +0000

    Simple form with validations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 var app = angular.module("myApp", []);
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: "templates/home.html",
+    controller: 'HomeController'
+  })
+  .when('/settings', {
+    templateUrl: "templates/settings.html",
+    controller: 'SettingsController'
+  })
+  .otherwise({ redirectTo: '/' });
+});
+
+app.controller('HomeController', function($scope) {
+
+});
+
+app.controller('SettingsController', function($scope) {
+  $scope.name = "Albert";
+});
